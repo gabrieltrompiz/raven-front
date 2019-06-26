20c1a00e4fa6adac88660ee7b57f8284c33232f3
Gabriel Trompiz <gab.tc@icloud.com>
2019-06-25 23:37:59 -0400

    fixed linear function that affects search bar container border radius
    added comments

--- views/Chats.tsx
+++ views/Chats.tsx
@@ -4,25 +4,29 @@ import AppHeader from '../components/AppHeader';
 import { SearchBar } from 'react-native-elements';
 
 const Chats: React.FC = () => {
-  const [scrolled, setScrolled] = useState(false)
+  const [scrolled, setScrolled] = useState(false) // state for controlling header's shadow
+  const [roundDelta, setDelta] = useState(1) // state for controlling border radius delta
 
-  const ref = useRef(null)
+  const ref = useRef(null) // ScrollView ref
 
   const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
-     if(Math.trunc(event.nativeEvent.contentOffset.y) >= 63 && !scrolled) {
-      setScrolled(true)
+    if(event.nativeEvent.contentOffset.y >= 0) {
+      setDelta(((-1/58) * event.nativeEvent.contentOffset.y) + 1) // linear function to generate border radius delta
+    }
+    if(Math.trunc(event.nativeEvent.contentOffset.y) >= 63 && !scrolled) {
+      setScrolled(true) // show header's shadow
     }
     else if(Math.trunc(event.nativeEvent.contentOffset.y) < 63 && scrolled) {
-      setScrolled(false)
+      setScrolled(false) // hide header's shadow
     }
   }
 
   const onRelease = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
     if(Math.trunc(event.nativeEvent.contentOffset.y) >= 28) {
-      ref.current.scrollTo({ x: 0, y: 68, animated: true })
+      ref.current.scrollTo({ x: 0, y: 68, animated: true }) // scroll to place after search bar if half of it is scrolled
     }
     else if(Math.trunc(event.nativeEvent.contentOffset.y) < 28)  {
-      ref.current.scrollTo({ x: 0, y: 0, animated: true })
+      ref.current.scrollTo({ x: 0, y: 0, animated: true }) // scroll to top if half of search bar is scrolled (upper)
     }
   }
 
@@ -32,7 +36,7 @@ const Chats: React.FC = () => {
       <ScrollView style={{ flex: 1 }} onScrollEndDrag={(event) => onRelease(event)} scrollEventThrottle={10} onScroll={(event) => onScroll(event)} ref={ref}>
         <SearchBar placeholder="Search" 
           containerStyle={{ backgroundColor: 'white', borderTopWidth: 0, borderBottomWidth: 0, shadowColor: '#000', shadowOffset: { height: 10, width: 0 }, 
-            shadowOpacity: !scrolled ? 0.05 : 0, shadowRadius: 5, borderBottomLeftRadius: 30, borderBottomRightRadius: 30, marginBottom: 10, paddingTop: 0 }}
+            shadowOpacity: !scrolled ? 0.05 : 0, shadowRadius: 5, borderBottomLeftRadius: 30  * roundDelta, borderBottomRightRadius: 30  * roundDelta, marginBottom: 10, paddingTop: 0 }}
           inputStyle={{ backgroundColor: '#F5F4FA' }}
           inputContainerStyle={{ backgroundColor: '#F5F4FA', alignSelf: 'center', borderRadius: 10, marginBottom: 10, width: '95%' }}
         />
